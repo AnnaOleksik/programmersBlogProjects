1b2a9b28fa71820120320f02f6d4a3f7c4b07ae3
Ania <greganmaria@gmail.com>
2021-05-22 14:54:27 +0200

    created and registered POst model

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 class Post(models.Model):
-    pass
+    title=models.CharField(max_length=200)
+    text=models.TextField()
+    createDate=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title + " " + str(self.createDate)
