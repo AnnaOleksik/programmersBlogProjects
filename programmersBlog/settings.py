fbe32088146b181e52c4062c847a5ae355ec2fb9
Ania <greganmaria@gmail.com>
2021-05-24 13:17:00 +0200

    image added to post model

--- programmersBlog/settings.py
+++ programmersBlog/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 #DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+MEDIA_ROOT=BASE_DIR/'media'
+MEDIA_URL='/media/'
